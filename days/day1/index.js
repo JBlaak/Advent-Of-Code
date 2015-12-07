3b523edc2602ad3e82884b7053ded17e233b1d67
Joris Blaak <joris@label305.com>
2015-12-07 18:32:29 +0100

    Day 1

---
+++ days/day1/index.js
@@ -0,0 +1,35 @@
+'use strict';
+import expect from 'expect';
+import {readFileSync} from 'fs';
+
+
+function notQuiteLisp(input) {
+    return input.split('').reduce((p, c) => {
+        switch (c) {
+            case '(':
+                return p + 1;
+                break;
+            case ')':
+                return p - 1;
+                break;
+            default:
+                throw 'Invalid char';
+        }
+    }, 0);
+}
+
+/* Examples */
+expect(notQuiteLisp('(())')).toBe(0);
+expect(notQuiteLisp('()()')).toBe(0);
+expect(notQuiteLisp('(((')).toBe(3);
+expect(notQuiteLisp('(()(()(')).toBe(3);
+expect(notQuiteLisp('))(((((')).toBe(3);
+expect(notQuiteLisp('())')).toBe(-1);
+expect(notQuiteLisp('))(')).toBe(-1);
+expect(notQuiteLisp(')))')).toBe(-3);
+expect(notQuiteLisp(')())())')).toBe(-3);
+
+/* From input */
+let input = readFileSync('./days/day1/input.txt', 'utf8');
+console.log(notQuiteLisp(input));
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+Advent of code
+====
+
+All coding challenges of adventofcode.com in JS
+
+Running
+----
+
+We're using Babel so:
+
+```
+./node_modules/.bin/babel-node ./days/day1/index.js
+```
