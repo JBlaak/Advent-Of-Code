9b547b4c617d4f03f8b71925e031dc1e42f671cc
Joris Blaak <joris@label305.com>
2015-12-13 16:58:16 +0100

    Part 2 of day 5

--- days/day5/index.js
+++ days/day5/index.js
@@ -4,7 +4,7 @@ import {readFileSync} from 'fs';
 
 // This can be done with one big if statement instead on three filters
 // but no human being could read that
-function weNeedzInterz(input) {
+function part1(input) {
     return input.split("\n")
         .map(s => s.split(''))
         // Vowels
@@ -16,14 +16,38 @@ function weNeedzInterz(input) {
         .length
 }
 
-/* Examples */
-expect(weNeedzInterz('ugknbfddgicrmopn')).toBe(1);
-expect(weNeedzInterz('aaa')).toBe(1);
-expect(weNeedzInterz('jchzalrnumimnmhp')).toBe(0);
-expect(weNeedzInterz('haegwjzuvuyypxyu')).toBe(0);
-expect(weNeedzInterz('dvszwmarrgswjxmb')).toBe(0);
+function part2(input) {
+    return input.split("\n")
+        .map(s => s.split(''))
+        //Repeating combies
+        .filter((chars) => chars.some((v, i, a) => {
+            let combi = v + a[i + 1];
+            return a.slice(i + 2).some((v, i, a) => v + a[i + 1] == combi);
+        }))
+        //Repeating
+        .filter(chars => chars.some((v, i, a) => v == a[i + 2]))
+        .length;
+}
+
+/* Examples part 1 */
+expect(part1('ugknbfddgicrmopn')).toBe(1);
+expect(part1('aaa')).toBe(1);
+expect(part1('jchzalrnumimnmhp')).toBe(0);
+expect(part1('haegwjzuvuyypxyu')).toBe(0);
+expect(part1('dvszwmarrgswjxmb')).toBe(0);
 
 /* From input */
-let input = readFileSync('./days/day5/input.txt', 'utf8');
-console.log(weNeedzInterz(input));
+let inputForPart1 = readFileSync('./days/day5/input.txt', 'utf8');
+console.log('Part 1: ' + part1(inputForPart1));
+
+/* Examples part 2 */
+expect(part2('qjhvhtzxzqqjkmpb')).toBe(1);
+expect(part2('xxyxx')).toBe(1);
+expect(part2('aaa')).toBe(0);
+expect(part2('uurcxstgmygtbstg')).toBe(0);
+expect(part2('ieodomkazucvgmuy')).toBe(0);
+
+/* From input */
+let inputForPart2 = readFileSync('./days/day5/input.txt', 'utf8');
+console.log('Part 2: ' + part2(inputForPart2));
 
