65dcada6a16be9263be4b549a97934f30436d59e
Joris Blaak <joris@label305.com>
2015-12-13 16:30:19 +0100

    Part 2 of day 4

--- days/day4/index.js
+++ days/day4/index.js
@@ -3,17 +3,20 @@ import expect from 'expect';
 import crypto from 'crypto';
 import {readFileSync} from 'fs';
 
-function theIdealStockingStuffer(input) {
+function exercise(input, prefix) {
     let value = -1;
     do
         value++;
-    while (!crypto.createHash('md5').update(input + String(value)).digest("hex").startsWith('00000'));
+    while (!crypto.createHash('md5').update(input + String(value)).digest("hex").startsWith(prefix));
     return value;
 }
 
-/* Examples */
-expect(theIdealStockingStuffer('abcdef')).toBe(609043);
-expect(theIdealStockingStuffer('pqrstuv')).toBe(1048970);
+/* Examples part 1 */
+expect(exercise('abcdef', '00000')).toBe(609043);
+expect(exercise('pqrstuv', '00000')).toBe(1048970);
 
-/* From input */
-console.log(theIdealStockingStuffer('yzbqklnj'));
+/* From input part 1 */
+console.log('Part 1: ' + exercise('yzbqklnj', '00000'));
+
+/* From input part 2 */
+console.log('Part 2: ' + exercise('yzbqklnj', '000000'));
