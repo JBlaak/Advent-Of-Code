104c7f47421196e8fb9297e140f7c5a683a8fb4a
Joris Blaak <joris@label305.com>
2015-12-13 14:50:03 +0100

    Part 2 of day 1

--- days/day1/index.js
+++ days/day1/index.js
@@ -2,33 +2,52 @@
 import expect from 'expect';
 import {readFileSync} from 'fs';
 
-function notQuiteLisp(input) {
-    return input.split('').reduce((p, c) => {
-        switch (c) {
-            case '(':
-                return p + 1;
-                break;
-            case ')':
-                return p - 1;
-                break;
-            default:
-                throw 'Invalid char';
-        }
-    }, 0);
+function getAddition(c) {
+    var addition = 0;
+    switch (c) {
+        case '(':
+            addition = 1;
+            break;
+        case ')':
+            addition = -1;
+            break;
+        default:
+            throw 'Invalid char';
+    }
+    return addition;
 }
 
-/* Examples */
-expect(notQuiteLisp('(())')).toBe(0);
-expect(notQuiteLisp('()()')).toBe(0);
-expect(notQuiteLisp('(((')).toBe(3);
-expect(notQuiteLisp('(()(()(')).toBe(3);
-expect(notQuiteLisp('))(((((')).toBe(3);
-expect(notQuiteLisp('())')).toBe(-1);
-expect(notQuiteLisp('))(')).toBe(-1);
-expect(notQuiteLisp(')))')).toBe(-3);
-expect(notQuiteLisp(')())())')).toBe(-3);
+function part1(input) {
+    return input.split('').reduce((p, c) => getAddition(c) + p, 0);
+}
+
+function part2(input) {
+    let p = 0;
+    return input.split('').findIndex(c => {
+            p += getAddition(c);
+            return p < 0;
+        }) + 1;
+}
+
+/* Examples part 1 */
+expect(part1('(())')).toBe(0);
+expect(part1('()()')).toBe(0);
+expect(part1('(((')).toBe(3);
+expect(part1('(()(()(')).toBe(3);
+expect(part1('))(((((')).toBe(3);
+expect(part1('())')).toBe(-1);
+expect(part1('))(')).toBe(-1);
+expect(part1(')))')).toBe(-3);
+expect(part1(')())())')).toBe(-3);
 
 /* From input */
-let input = readFileSync('./days/day1/input.txt', 'utf8');
-console.log(notQuiteLisp(input));
+let inputForPart1 = readFileSync('./days/day1/input.txt', 'utf8');
+console.log('Part 1: ' + part1(inputForPart1));
+
+/* Examples part 2 */
+expect(part2(')')).toBe(1);
+expect(part2('()())')).toBe(5);
+
+let inputForPart2 = readFileSync('./days/day1/input.txt', 'utf8');
+console.log('Part 2: ' + part2(inputForPart2));
 
